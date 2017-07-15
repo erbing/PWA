bd44bba0ca85b49051750cc1b741c8d367d227ac
zigzga <447587073@qq.com>
2017-07-15 16:51:25 +0800

    pwa base

---
+++ src/entry-skeleton.js
@@ -0,0 +1,14 @@
+/**
+ * @file 页面骨架
+ * @author zigzga(447587073@qq.com)
+ */
+
+import Vue from 'vue';
+import Skeleton from '@/pages/Skeleton';
+
+export default new Vue({
+    components: {
+        Skeleton
+    },
+    template: '<skeleton />'
+});
